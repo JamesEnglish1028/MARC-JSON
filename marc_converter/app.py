36572a1b0e04deb00bd9cb163dbf29b80c381a27
James English <jce1028@gmail.com>
2025-10-16 08:40:38 -0400

    Enable CORS for all routes using flask-cors

--- marc_converter/app.py
+++ marc_converter/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
+CORS_origins = '*'
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes and origins (for dev)
 
 # Import routes to register them with the app
 import marc_converter.views
